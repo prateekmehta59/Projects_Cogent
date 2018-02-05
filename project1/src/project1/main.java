86e19a8b18a777c04a985020d35dc3fe1e9102ea
Prateek Mehta <pmehta59@bu.edu>
2018-02-05 12:21:54 -0500

    Added Project1

---
+++ project1/src/project1/main.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		newEmployee emp1 = new newEmployee("prateek", "mehta", "sales");
+		System.out.println(emp1.getEmail());
+		System.out.println(emp1.getPassword());
+	}
+
+}
---
+++ project1/src/project1/newEmployee.java
@@ -0,0 +1,83 @@
+package project1;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class newEmployee {
+	private String firstName;
+	private String lastName;
+//	private String email;
+	private String altEmail;
+	private Integer mailboxCapacity;
+	private String password;
+	private static Integer passwordLen = 6;
+	private String department;
+	private static String company = "XYZ";
+	
+	static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	static SecureRandom rnd = new SecureRandom();
+	
+	public newEmployee(String firstName, String lastName, String department) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		
+		if (Arrays.asList("sales", "development", "accounting").contains(department.toLowerCase())) {
+			this.department = department;
+		} else {
+			this.department = "";
+		}
+	}
+
+	public String newEmail() {
+		return firstName+ "." + lastName + "@" + department + "." + company + ".com";
+	}
+	
+	public String newPassword() {
+		StringBuilder sb = new StringBuilder( passwordLen );
+		   for( int i = 0; i < passwordLen; i++ ) 
+		      sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+		   return sb.toString();
+	}
+	
+	public String getEmail() {
+		return this.newEmail();
+	}
+
+
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+
+
+	public String getAltEmail() {
+		return altEmail;
+	}
+
+	public void setAltEmail(String altEmail) {
+		this.altEmail = altEmail;
+	}
+
+	
+	public Integer getMailboxCapacity() {
+		return mailboxCapacity;
+	}
+
+	public void setMailboxCapacity(Integer mailboxCapacity) {
+		this.mailboxCapacity = mailboxCapacity;
+	}
+
+	
+	public String getPassword() {
+		return this.newPassword();
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+}
